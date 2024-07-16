45f63c22e6ae959825258362aefc53816804db6a
hminaee-tc <hminaee@tucows.com>
2024-07-16 14:26:37 -0300

    feat/util-sync-with-upstream cleanup

--- hooks/utils/fork_utils.sh
+++ hooks/utils/fork_utils.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 
-decide_sync_strategy_based_on_fork_status() {
+get_upstream() {
   local fork_status_local="{}"
-  # if [[ $CI == "true" ]]; then
-  #   # Running in CI environment
-  #   log "INFO" "$REPO_NAME is the repo name"
-  #   fork_status_local=$(fetch_fork_parent_repo_info "$REPO_NAME")
-  # else
-  #   # Running in local environment
-  #   if [ -f ".github/UPSTREAM" ]; then
-  #     log "INFO" "Using .github/UPSTREAM file for fork status."
-  #     fork_status_local=$(cat ".github/UPSTREAM")
-  #   else
-  #     log "WARN" "No .github/UPSTREAM file found. Exiting."
-  #     echo "{}"
-  #   fi
-  # fi
   if [ -f ".github/UPSTREAM" ]; then
     log "INFO" "Using .github/UPSTREAM file for fork status."
     fork_status_local=$(cat ".github/UPSTREAM")
@@ -23,8 +9,52 @@ decide_sync_strategy_based_on_fork_status() {
     log "WARN" "No .github/UPSTREAM file found. Please check the PR in the repo. If you have pushed your changes and created the PR there should be a PR creating UPSTREAM file."
     echo "{}"
   fi
-  # return $fork_status_local
   echo "$fork_status_local"
+}
+
+sync_fork_with_upstream_branch() {
+  local fork_status_local=$1
+  local upstream_branch="feat/add-initial-sync-flow"
+
+  # Remove unnecessary escape characters
+  fork_status_local=$(echo "${fork_status_local}" | sed 's/\\\"/\"/g')
+  log "DEBUG" "Parsing fork_status_local with jq: ${fork_status_local}"
+
+  local upstream_repo=$(echo "${fork_status_local}" | jq -r '.parent')
+  if ! git remote get-url upstream &>/dev/null; then
+    git remote add upstream "git@github.com:${upstream_repo}.git"
+  fi
+
+  local fetch_output=$(git fetch upstream 2>&1)
+  local fetch_exit_status=$?
+
+  if [[ $fetch_exit_status -ne 0 ]]; then
+    log "ERROR" "Error fetching upstream changes: $fetch_output"
+    exit 1
+  else
+    log "INFO" "Successfully fetched upstream changes."
+  fi
+
+  if git diff --quiet HEAD upstream/${upstream_branch}; then
+    log "INFO" "Branch is up-to-date with 'upstream/${upstream_branch}'."
+    exit 0
+  else
+    local merge_output=$(git merge --no-edit upstream/${upstream_branch} 2>&1)
+    local merge_exit_status=$?
+
+    if [[ $merge_exit_status -eq 0 ]]; then
+      if [[ $merge_output == *"Already up to date."* ]]; then
+        log "INFO" "No changes were necessary; your branch was already up to date."
+        exit 0
+      else
+        log "INFO" "Merge successful. Precommit will exit with error though as the branch was not synced with upstream. Rerun the precommit to check if everything is ready to push."
+        exit 1
+      fi
+    else
+      log "ERROR" "Failed to automatically sync with 'upstream/${upstream_branch}'. Please resolve conflicts manually or run pre-commit run --all locally."
+      exit 1
+    fi
+  fi
 }
 
 log() {
