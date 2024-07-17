69b0aff19e21909bf9277c553b1c786d0ca42399
hminaee-tc <hminaee@tucows.com>
2024-07-17 10:44:25 -0300

    feat/util-sync-with-upstream rfactor code

--- hooks/utils/fork_utils.sh
+++ hooks/utils/fork_utils.sh
@@ -12,15 +12,63 @@ get_upstream() {
   echo "$fork_status_local"
 }
 
+
+ensure_jq_installed() {
+  if ! command -v jq &>/dev/null; then
+    log "ERROR" "jq is not installed. Please install jq to parse JSON data."
+    exit 1
+  fi
+}
+
+parse_upstream_repo() {
+  local upstream_local=$1
+  upstream_local=$(echo "${upstream_local}" | sed 's/\\\"/\"/g')
+  log "DEBUG" "Parsing upstream_local with jq: ${upstream_local}"
+  echo "${upstream_local}" | jq -r '.parent'
+}
+
+add_remote_upstream() {
+  local upstream_repo=$1
+  if ! git remote get-url upstream &>/dev/null; then
+    git remote add upstream "git@github.com:${upstream_repo}.git"
+  fi
+}
+
+fetch_upstream_changes() {
+  if ! git fetch upstream 2>&1; then
+    log "ERROR" "Error fetching upstream changes."
+    exit 1
+  else
+    log "INFO" "Successfully fetched upstream changes."
+  fi
+}
+
+merge_upstream_changes() {
+  local upstream_branch=$1
+  if git diff --quiet HEAD upstream/${upstream_branch}; then
+    log "INFO" "Branch is up-to-date with 'upstream/${upstream_branch}'."
+    exit 0
+  else
+    if git merge --no-edit upstream/${upstream_branch} 2>&1; then
+      log "INFO" "Merge successful. Branch is synced with upstream."
+      exit 0
+    else
+      log "ERROR" "Failed to automatically sync with 'upstream/${upstream_branch}'."
+      exit 1
+    fi
+  fi
+}
+
 sync_fork_with_upstream_branch() {
   local fork_status_local=$1
   local upstream_branch="feat/add-initial-sync-flow"
 
   # Remove unnecessary escape characters
-  fork_status_local=$(echo "${fork_status_local}" | sed 's/\\\"/\"/g')
-  log "DEBUG" "Parsing fork_status_local with jq: ${fork_status_local}"
+  # fork_status_local=$(echo "${fork_status_local}" | sed 's/\\\"/\"/g')
+  # log "DEBUG" "Parsing fork_status_local with jq: ${fork_status_local}"
 
-  local upstream_repo=$(echo "${fork_status_local}" | jq -r '.parent')
+  # local upstream_repo=$(echo "${fork_status_local}" | jq -r '.parent')
+  local upstream_repo=$(parse_upstream_repo "${upstream_local}")
   if ! git remote get-url upstream &>/dev/null; then
     git remote add upstream "git@github.com:${upstream_repo}.git"
   fi
