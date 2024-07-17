26168b5533e01ba204bdcec53f1154b334f51e75
hminaee-tc <hminaee@tucows.com>
2024-07-17 10:30:52 -0300

    feat/util-sync-with-upstream rfactor code

--- hooks/utils/fork_utils.sh
+++ hooks/utils/fork_utils.sh
@@ -18,38 +18,36 @@ get_upstream() {
   echo "$upstream_local"
 }
 
-ensure_jq_installed() {
+sync_fork_with_upstream_branch() {
+  local upstream_local=$1
+  local upstream_branch="feat/add-initial-sync-flow"
+
+  # Ensure jq is available
   if ! command -v jq &>/dev/null; then
     log "ERROR" "jq is not installed. Please install jq to parse JSON data."
     exit 1
   fi
-}
 
-parse_upstream_repo() {
-  local upstream_local=$1
   upstream_local=$(echo "${upstream_local}" | sed 's/\\\"/\"/g')
   log "DEBUG" "Parsing upstream_local with jq: ${upstream_local}"
-  echo "${upstream_local}" | jq -r '.parent'
-}
 
-add_remote_upstream() {
-  local upstream_repo=$1
+  local upstream_repo=$(echo "${upstream_local}" | jq -r '.parent')
+  if [[ -z "$upstream_repo" ]]; then
+    log "ERROR" "Failed to parse upstream repository from fork status."
+    exit 1
+  fi
+
   if ! git remote get-url upstream &>/dev/null; then
     git remote add upstream "git@github.com:${upstream_repo}.git"
   fi
-}
 
-fetch_upstream_changes() {
   if ! git fetch upstream 2>&1; then
     log "ERROR" "Error fetching upstream changes."
     exit 1
   else
     log "INFO" "Successfully fetched upstream changes."
   fi
-}
 
-merge_upstream_changes() {
-  local upstream_branch=$1
   if git diff --quiet HEAD upstream/${upstream_branch}; then
     log "INFO" "Branch is up-to-date with 'upstream/${upstream_branch}'."
     exit 0
@@ -62,21 +60,4 @@ merge_upstream_changes() {
       exit 1
     fi
   fi
-}
-
-sync_fork_with_upstream_branch() {
-  local upstream_local=$1
-  local upstream_branch="feat/add-initial-sync-flow"
-
-  ensure_jq_installed
-
-  local upstream_repo=$(parse_upstream_repo "${upstream_local}")
-  if [[ -z "$upstream_repo" ]]; then
-    log "ERROR" "Failed to parse upstream repository from fork status."
-    exit 1
-  fi
-
-  add_remote_upstream "${upstream_repo}"
-  fetch_upstream_changes
-  merge_upstream_changes "${upstream_branch}"
 }
