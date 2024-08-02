f0e9d73f3f59bfef8c38d4b29c4404108edb5349
hminaee-tc <hminaee@tucows.com>
2024-08-02 16:35:53 -0300

    feat/checkcommitmsg: commit linting (#8)
    feat/checkcommitmsg commit msg

---
+++ hooks/setup-commit-msg-hook.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+# Source fork-utils.sh using the calculated directory
+source "${SCRIPT_DIR}/utils/fork_utils.sh"
+# Exit the script if running in a CI environment
+if [ "$CI" == "true" ]; then
+    exit 0
+fi
+
+# Path to the commit-msg hook
+HOOK_PATH=".git/hooks/commit-msg"
+
+rm -rf $HOOK_PATH || true
+
+# Desired content of the commit-msg hook
+read -r -d '' HOOK_CONTENT <<'EOF'
+#!/bin/bash
+
+# Function to check branch name
+check_branch_name() {
+    # Get the current branch name
+    BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+
+    # Regular expression for the required branch name format
+    BRANCH_PATTERN="^(feat|fix|build|breaking|chore|ci|docs|perf|refactor|revert|test)\/[a-zA-Z0-9-]+$"
+
+    # Check if the branch name matches the pattern
+    if [[ ! "$BRANCH_NAME" =~ $BRANCH_PATTERN ]]; then
+        echo "ERROR: Branch name does not follow the required format 'git branch -m type/jira-number'. Type can be any of feat, fix, build, breaking, chore, ci, docs, perf, refactor, revert, test"
+        exit 1
+    else
+        echo "INFO: Branch name matches the pattern"
+    fi
+}
+
+# Check the branch name before allowing the commit
+check_branch_name
+
+# Path to the file containing the commit message
+COMMIT_MSG_FILE=$1
+
+# Regular expression for the required commit message format
+PATTERN="^(feat|fix|build|breaking|chore|ci|docs|perf|refactor|revert|test)\/([a-zA-Z0-9-]+)(:)? *.+$"
+
+# Read the commit message
+COMMIT_MSG=$(cat "$COMMIT_MSG_FILE")
+
+# Check if the commit message matches the pattern
+if [[ ! "$COMMIT_MSG" =~ $PATTERN ]]; then
+    echo "ERROR: Commit message does not follow the required format 'type/jira-ticket: description. type can be any of feat, fix, build, breaking, chore, ci, docs, perf, refactor, revert, test'"
+    exit 1
+fi
+
+
+# If the script reaches this point, the commit message is valid
+exit 0
+EOF
+
+# Write the desired content to the commit-msg hook, creating or updating it
+echo "$HOOK_CONTENT" >"$HOOK_PATH"
+
+# Make the hook executable
+chmod +x "$HOOK_PATH"
